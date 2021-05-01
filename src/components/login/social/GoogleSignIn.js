4bc538a460113d77806a03a3abb1641d5ca1827a
suropark <52706443+suropark@users.noreply.github.com>
2021-05-01 14:11:29 +0900

    [ 박수로 ] 버그픽스 : 로그인 시 로그아웃버튼으로 안바뀌는 버그

--- src/components/login/social/GoogleSignIn.js
+++ src/components/login/social/GoogleSignIn.js
@@ -5,25 +5,27 @@ import axios from 'axios';
 import { PUBLIC_IP } from '../../../config';
 import { message } from 'antd';
 
-function GoogleSignIn ({ setModalVisible }) {
+function GoogleSignIn({ setModalVisible, setLogin }) {
   const history = useHistory();
   return (
     <>
       <GoogleLogin
         clientId="13311386829-vlj3ciu02fu1tqriq8dqo0a3nsm4f90u.apps.googleusercontent.com"
         onSuccess={(googleData) => {
-          console.log(googleData);
-          console.log(googleData.profileObj.email);
           axios
-            .post(`${PUBLIC_IP}/user/sign-in`, {
-              email: googleData.profileObj.email,
-              provider: 'google',
-            }, {withCredentials : true})
+            .post(
+              `${PUBLIC_IP}/user/sign-in`,
+              {
+                email: googleData.profileObj.email,
+                provider: 'google',
+              },
+              { withCredentials: true },
+            )
             .then((response) => {
               if (response.status === 200) {
-                console.log('this?', response);
                 message.success('로그인이 정상 완료 되었습니다.');
                 history.push('/');
+                setLogin(true);
                 setModalVisible(false);
               }
             })
