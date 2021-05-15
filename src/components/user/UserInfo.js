74f2caed92535890f384b6f02b637c31c86237be
suropark <52706443+suropark@users.noreply.github.com>
2021-05-15 20:09:34 +0900

    [ 박수로 ] 에러픽스 : 마이페이지 웹메일, 로그아웃 렌더링

--- src/components/user/UserInfo.js
+++ src/components/user/UserInfo.js
@@ -39,7 +39,10 @@ function UserInfo(props) {
 
     if (answer) {
       const body = getChangedInfo();
-      console.log(body);
+      if (body == undefined) {
+        message.info('변경 사항이 없습니다.');
+        return;
+      }
       dispatch(updateUser(body))
         .then((response) => {
           message.success('정보 변경이 완료되었습니다.');
@@ -146,7 +149,8 @@ function UserInfo(props) {
                 ) : (
                   <>
                     <Input
-                      value={webMail}
+                      defaultValue={webMail}
+                      value={webMailInput}
                       onChange={(e) => setWebMailInput(e.target.value)}
                       style={{ width: '200px' }}
                       suffix={<>@hufs.ac.kr</>}
