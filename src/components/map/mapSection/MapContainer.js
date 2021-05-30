447ff6290e8fe34d6b601d9c81d4434031f54050
xogns9578 <xogns9578@gmail.com>
2021-05-31 02:14:42 +0900

    [ 이태훈 ] 주거공간 생성

--- src/components/map/mapSection/MapContainer.js
+++ src/components/map/mapSection/MapContainer.js
@@ -2,10 +2,7 @@ import React, { useState, useEffect } from 'react';
 import KaKaoMap from './KakaoMap';
 import Card from './Card.js';
 import SearchBar from './SearchBar.js';
-import mapboo from '../../../image/boo/mapboo.png';
-import { Button } from 'antd';
-import axios from 'axios'
-import { PUBLIC_IP } from '../../../config';
+import MapNavi from '../component/MapNavi';
 
 
 
@@ -14,32 +11,8 @@ const MapContainer = ({ match }) => {
   const [lat, setLat] = useState(37.59732049638715); // default 서울캠
   const [lng, setLng] = useState(127.0588283395548);
   const [keyword, setKeyword] = useState('');
-  const [seoul, setSeoul] = useState();
-  const [global, setGlobal] = useState();
+
   var check = true;
-
-
-  useEffect(async () => {
-    // const seoul = 
-    axios.get(`${PUBLIC_IP}/store/seoul`)
-      .then((res) => {
-        setData(res.data);
-        setSeoul(res.data)
-
-      })
-
-
-  }, [])
-  useEffect(async () => {
-    // const global = 
-    axios.get(`${PUBLIC_IP}/store/global`)
-      .then((res) => setGlobal(res.data))
-    // setGg(global)
-  }, [])
-  //  초기에 한번씩만 받기위해
-  // console.log(data)
-
-
 
   const handleValueChange = (e) => {
     setKeyword(e.target.value);
@@ -83,38 +56,9 @@ const MapContainer = ({ match }) => {
 
   return (
     <div className="Map">
-      <div className="Map-left">
-        <div className="content" >
 
-          <div id="seoul" defaultSelectedKeys={['1']}
-            onClick={(e) => {
-              setData(seoul);
-              setKeyword('')
-              setLat(37.59732049638715);
-              setLng(127.0588283395548)
-            }}>
-            <div type="text" id="button-head" key="1">Seoul</div>
-            <Button type="text"  >맛집 공간</Button>
+      <MapNavi setData={setData} setLng={setLng} setLat={setLat} setKeyword={setKeyword} />
 
-          </div>
-          <hr className="line" />
-          <div id="global" defaultSelectedKeys={['2']}>
-            <div type="text" id="button-head" key="2">Global</div>
-            <Button type="text" onClick={(e) => {
-              setData(global);
-              setKeyword('')
-              setLat(37.336538181222245);
-              setLng(127.25253858610613);
-            }}>맛집 공간</Button>
-
-          </div>
-
-        </div>
-        <div className="map-boo">
-          <img src={mapboo} alt="숨어 있는 부" />
-        </div>
-      </div>
-      <div className="up-down" />
       <div className="Map-main">
         <div id="KaKaoMap">
           <KaKaoMap lat={lat} lng={lng} />
