e1a288ef836198ee9a104e7b058abc4c4e5d2033
free-t <63388488+free-t@users.noreply.github.com>
2021-05-31 23:16:59 +0900

    [ 이태훈 ] 기능추가 : 주거공간

---
+++ src/components/map/house/House.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import MapNavi from '../component/MapNavi'
+function House() {
+
+
+    return (
+        <>
+            <MapNavi />
+
+        </>
+    )
+
+}
+
+export default House;
