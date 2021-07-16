eefb2db3ee3ab76d9cbba8b57a3863acff703856
suropark <xoopp5818@gmail.com>
2021-07-16 22:59:57 +0900

    [ 박수로 ] 에러픽스 :  검색 관련 에러 수정

--- src/components/post/SearchAll.js
+++ src/components/post/SearchAll.js
@@ -2,47 +2,27 @@ import React, { useState } from 'react';
 import { message, Select, Input } from 'antd';
 import useInput from '../../hooks/useInput';
 import { postSearch } from '../../functions/postFunctions';
-import { withRouter } from 'react-router-dom';
+import { useHistory, withRouter } from 'react-router-dom';
 import useResponsive from '../../hooks/useResponsive';
+import useErrorHandling from '../../hooks/useErrorHandling';
 const { Option } = Select;
 const { Search } = Input;
 function SearchAll(props) {
-  const [keyword, setKeyword] = useInput('');
+  const history = useHistory();
+  const errorHandling = useErrorHandling();
+
   const { Mobile, Default } = useResponsive();
-  const onSearch = () => {
+  const onSearch = (keyword) => {
     if (keyword === '') {
       return message.warn('검색 키워드를 입력해주세요.');
     }
     const body = { keyword: keyword, option: 'titleAndContent' };
     postSearch(body)
-      .then((response) => {
-        props.history.push({
-          pathname: `/search`,
-          state: { detail: response.payload.reverse() },
-        });
+      .then((res) => {
+        history.push(`/search`, { detail: res });
       })
       .catch((error) => {
-        switch (error.response?.status) {
-          case 401:
-            message.error('로그인하지 않은 사용자');
-            // history.push('/');
-            break;
-          case 403:
-            message.error('접근 권한 오류');
-            // history.push('/');
-            break;
-          case 404:
-            message.info('검색 결과가 존재하지 않습니다.');
-            break;
-          case 422:
-            if (error.response.data.message === 'QUERY_KEYWORD') {
-              message.error('두 글자 이상 입력해주세요');
-            } else {
-              message.error('query error');
-            }
-          default:
-            break;
-        }
+        errorHandling(error.response?.data.message);
       });
   };
   return (
@@ -50,9 +30,7 @@ function SearchAll(props) {
       <Mobile>
         <Search
           allowClear
-          value={keyword}
-          onChange={setKeyword}
-          onSearch={onSearch}
+          onSearch={(e) => onSearch(e)}
           type="text"
           style={{ paddingTop: 30, paddingLeft: 40, paddingRight: 40 }}
           placeholder="검색어를 입력해주세요!"
@@ -63,9 +41,9 @@ function SearchAll(props) {
         <span id="Searchbar">
           <Search
             allowClear
-            value={keyword}
-            onChange={setKeyword}
-            onSearch={onSearch}
+            onSearch={(e) => {
+              onSearch(e);
+            }}
           />
         </span>
       </Default>
