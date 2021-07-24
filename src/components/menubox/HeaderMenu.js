9cd6a41bff664c67a5d57d0f7f8fb884f3c0240e
xogns9578 <xogns9578@gmail.com>
2021-07-25 02:34:24 +0900

    [ 이태훈 ] seo, open graph 적용

--- src/components/menubox/HeaderMenu.js
+++ src/components/menubox/HeaderMenu.js
@@ -34,7 +34,7 @@ function HeaderMenu(props) {
           자유 공간
         </Link>
       </Menu.Item>
-      <Menu.Item>
+      {/* <Menu.Item>
         <Link to="/board/질문공간" onClick={onClose}>
           질문 공간
         </Link>
@@ -43,7 +43,7 @@ function HeaderMenu(props) {
         <Link to="/board/공구공간" onClick={onClose}>
           공구 공간
         </Link>
-      </Menu.Item>
+      </Menu.Item> */}
     </Menu>
   );
   const menu2 = (
