df388f6f0849381a9be200ab24ac5598f88cce32
suropark <xoopp5818@gmail.com>
2021-07-24 20:40:49 +0900

    [ 박수로 ] 기능 수정 : 장학금 페이지 이메일 인증 제한

--- src/views/Common/Header.js
+++ src/views/Common/Header.js
@@ -3,27 +3,11 @@ import logo from '../../image/logo.png';
 // import mainboo from '../../banner/mainboo.png';
 import { Menu, Dropdown, Button, Space, Input, message } from 'antd';
 import { Redirect, withRouter } from 'react-router';
-import { Link } from 'react-router-dom';
-import SignUp from '../../components/login/SignUp';
-import Logout from '../../components/login/Logout';
-import Cookies from 'js-cookie';
-import axios from 'axios';
-import { PUBLIC_IP } from '../../config';
 import HeaderMenu from '../../components/menubox/HeaderMenu';
 const { Search } = Input;
 
 function Header(props) {
   const [login, setLogin] = useState(false);
-  useEffect(async () => {
-    await axios
-      .get(`${PUBLIC_IP}/user`)
-      .then((response) => {
-        setLogin(true);
-      })
-      .catch((error) => {
-        setLogin(false);
-      });
-  }, []);
 
   return <HeaderMenu setLogin={setLogin} login={login} />;
 }
--- src/views/RegisterPage/SignInPage.js
+++ src/views/RegisterPage/SignInPage.js
@@ -2,26 +2,30 @@ import React, { useState } from 'react';
 import { Modal, Button } from 'antd';
 import GoogleSignIn from '../../components/login/social/GoogleSignIn';
 import KakaoSignIn from '../../components/login/social/KakaoSignIn';
-
-function SignInPage({ setLogin }) {
+import { mutate } from 'swr';
+import { PUBLIC_IP } from '../../config';
+function SignInPage() {
   const [modalVisible, setModalVisible] = useState(false);
-
   return (
     <>
-    <Button type="text" onClick={()=>setModalVisible(true)}>
-      로그인
-    </Button>
-    <Modal
-      title="로그인 / LOGIN"
-      centered
-      okButtonProps={{ style: { display: 'none' } }}
-      visible={modalVisible}
-      onOk={() => setModalVisible(false)}
-      onCancel={() => {setModalVisible(false)}}
-    >
-      <GoogleSignIn setLogin={setLogin} />
-      <KakaoSignIn setLogin={setLogin} />
-    </Modal>
+      <Button type="text" onClick={() => setModalVisible(true)}>
+        로그인
+      </Button>
+      <Modal
+        title="로그인 / LOGIN"
+        centered
+        okButtonProps={{ style: { display: 'none' } }}
+        visible={modalVisible}
+        onOk={() => {
+          setModalVisible(false);
+        }}
+        onCancel={() => {
+          setModalVisible(false);
+        }}
+      >
+        <GoogleSignIn />
+        <KakaoSignIn />
+      </Modal>
     </>
   );
 }
