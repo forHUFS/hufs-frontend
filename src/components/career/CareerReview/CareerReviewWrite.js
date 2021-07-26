52ac543dc37bdf946fce0d7474f70cb2b42161c6
Namju <48855763+NamNamju@users.noreply.github.com>
2021-07-27 02:32:46 +0900

    [ 황남주 ] 기능 수정 : 취창업공간, url수정 (#246)
    * [ 황남주 ] 기능수정 : boardTitle 수정 및 반응형
    
    * [ 기능 수정 ] 황남주 : 최신순리뷰, router 수정
    
    * [ 황남주 ] 기능 추가 : 취/창업공간
    
    * [ 황남주 ] 기능수정 : 취창업공간, URL 변경

--- src/components/career/CareerReview/CareerReviewWrite.js
+++ src/components/career/CareerReview/CareerReviewWrite.js
@@ -1,33 +1,86 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import { PUBLIC_IP } from '../../../config';
-import { Checkbox, Divider,PageHeader } from 'antd';
+import { Checkbox, Divider,PageHeader,message } from 'antd';
+import {
+  careerSave,
+  postRemove,
+} from '../../../_actions/reviewPost_action'
+import { useDispatch } from 'react-redux';
+import useResponsive from '../../../hooks/useResponsive'
+
+
 const CheckboxGroup = Checkbox.Group;
 
-const plainOptions = ['인문', '예술', 'IT'];
+const plainOptions = ['어문', '창업', 'IT'];
 const defaultCheckedList = [];
 
-function CareerReviewWrite() {
- const [state, setstate] = useState({title:"",contents:""});
+function CareerReviewWrite({location,history},props) {
+  console.log(history)
+  const { isMobile, Default, Mobile } = useResponsive();
+ const [state, setState] = useState({title: '',content: '',header: ''});
  const [checkedList, setCheckedList] = useState(defaultCheckedList);
  const [indeterminate, setIndeterminate] = useState(true);
 const [checkAll, setCheckAll] = useState(false);
+const dispatch = useDispatch();
 
   const onChange = list => {
+    console.log(list)
     setCheckedList(list);
     setIndeterminate(!!list.length && list.length < plainOptions.length);
     setCheckAll(list.length === plainOptions.length);
+    setState({...state,header:list})
   };
 
   const onCheckAllChange = e => {
     setCheckedList(e.target.checked ? plainOptions : []);
     setIndeterminate(false);
     setCheckAll(e.target.checked);
+    setState({...state,header:checkedList})
   };
 
-  const _submitBoard = async function () {
+  const onSubmit = (e) => {
+
+    console.log(state)
+    /* e.preventDefault(); */
+    if (state.title.trim().length === 0) {
+      // 공백 제목 검사
+      message.info('제목을 적어주세요');
+      return;
+    }
+    let body = {
+      title: state.title,
+      content: state.content,
+      header: state.header,
+    };
+    console.log(body)
+    console.log(location.pathname.substring(7,12))
+    dispatch(careerSave(body, history.location.state.detail))
+      .then((response) => {
+        if (response.status === 200) {
+          props.history.goBack();
+        }
+      })
+      .catch((error) => {
+        switch (error.response?.status) {
+          case 401:
+            message.error('로그인이 필요합니다.');
+            props.history.push('/');
+          case 403:
+            message.error('접근 권한 오류');
+            props.history.push('/');
+            break;
+          default:
+            break;
+        }
+      });
+  };
+
+
+  /* const _submitBoard = async function () {
     const title = document.getElementsByName('title')[0].value.trim();
     const contents = document.getElementsByName('contents')[0].value.trim();
+    const header = document.getElementsByName('header')[0].value;
   
     if (title === '') {
       return alert('제목을 입력해주세요');
@@ -35,8 +88,8 @@ const [checkAll, setCheckAll] = useState(false);
       return alert('내용을 입력해주세요');
     }
   
-    const data = { title: title, contents: contents };
-    const res = await axios(`${PUBLIC_IP}/board/${8}/post`, {
+    const data = { title: title, contents: contents, header:header };
+    const res = await axios(`${PUBLIC_IP}/board/careerReview`, {
       method: 'POST',
       data: data,
       headers: new Headers(),
@@ -44,38 +97,79 @@ const [checkAll, setCheckAll] = useState(false);
   
     if (res.data) {
       alert('글이 등록되었습니다.');
-      return window.location.replace('/career');
+      return window.location.replace('/board/취창업공간');
     }
   };
-  
+   */
 
   return (
     <>
+    <Mobile>
      <PageHeader
         title={'후기'}
       />
-      <div className="Career-Write-Main">
+      <div className="Career-Write-Main" style = {{marginTop:"-900px", width:"500px"}}>
         <div className="Career-Write">
-          <input type="text" id="title_txt" name="title" placeholder="제목" />
+          <input type="text" id="title_txt" name="title" value={state.title} onChange={(e) => {
+            console.log(e.target.value)
+            setState({...state,title: e.target.value });
+          }} placeholder="제목" />
           
         </div>
         <div className="Career-Check">
-        <Checkbox indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
+        <Checkbox name= "header" indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
         Check all
       </Checkbox>
       <Divider />
-      <CheckboxGroup options={plainOptions} value={checkedList} onChange={onChange} />
+      <CheckboxGroup options={plainOptions} value={checkedList || ""} onChange={onChange} />
       </div>
         <div>
           <textarea
             id="content_txt"
             name="contents"
             placeholder="내용을 입력하시오."
+            value={state.content} onChange={(e) => {
+              console.log(e.target.value)
+              setState({...state,content: e.target.value });}}
           ></textarea>
         </div>
 
-        <button onClick={()=>_submitBoard()}>포스트 등록</button>
+        <button style={{marginLeft:"10px"}} onClick={onSubmit}>포스트 등록</button>
       </div>
+      </Mobile>
+    <Default>
+     <PageHeader
+        title={'후기'}
+      />
+      <div className="Career-Write-Main">
+        <div className="Career-Write">
+          <input type="text" id="title_txt" name="title" value={state.title} onChange={(e) => {
+            console.log(e.target.value)
+            setState({...state,title: e.target.value });
+          }} placeholder="제목" />
+          
+        </div>
+        <div className="Career-Check">
+        <Checkbox name= "header" indeterminate={indeterminate} onChange={onCheckAllChange} checked={checkAll}>
+        Check all
+      </Checkbox>
+      <Divider />
+      <CheckboxGroup options={plainOptions} value={checkedList || ""} onChange={onChange} />
+      </div>
+        <div>
+          <textarea
+            id="content_txt"
+            name="contents"
+            placeholder="내용을 입력하시오."
+            value={state.content} onChange={(e) => {
+              console.log(e.target.value)
+              setState({...state,content: e.target.value });}}
+          ></textarea>
+        </div>
+
+       <button onClick={onSubmit}>포스트 등록</button>
+      </div>
+      </Default>
     </>
   );
 }
